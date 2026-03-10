6105a6459e282308dff7d20369118982d39c796b
DOMINIQUE EDUARDA SILVA DE JESUS <dominique.jesus@aluno.ifsp.edu.br>
2026-03-09 23:42:02 -0300

    Add files via upload

---
+++ Consultas - Número de registros.sql
@@ -0,0 +1,55 @@
+-- Atividade 5 - Quantidade de Registros por Tabela
+
+-- Tabela 1: advisor
+SELECT COUNT(*) as qntRegistros FROM PTBPABD.dbo.advisor;
+-- Número de Registros: 0
+
+
+-- Tabela 2: classroom
+SELECT COUNT(*) as qntRegistros FROM PTBPABD.dbo.classroom;
+-- Número de Registros: 30
+
+
+-- Tabela 3: course
+SELECT COUNT(*) as qntRegistros FROM PTBPABD.dbo.course;
+-- Número de Registros: 200
+
+
+-- Tabela 4: department
+SELECT COUNT(*) as qntRegistros FROM PTBPABD.dbo.department;
+-- Número de Registros: 20
+
+
+-- Tabela 5: instructor
+SELECT COUNT(*) as qntRegistros FROM PTBPABD.dbo.instructor;
+-- Número de Registros: 50
+
+
+-- Tabela 6: prereq
+SELECT COUNT(*) as qntRegistros FROM PTBPABD.dbo.prereq;
+-- Número de Registros: 0
+
+
+-- Tabela 7: section
+SELECT COUNT(*) as qntRegistros FROM PTBPABD.dbo.section;
+-- Número de Registros: 100
+
+
+-- Tabela 8: student
+SELECT COUNT(*) as qntRegistros FROM PTBPABD.dbo.student;
+-- Número de Registros: 2000
+
+
+-- Tabela 9: takes
+SELECT COUNT(*) as qntRegistros FROM PTBPABD.dbo.takes;
+-- Número de Registros: 17575
+
+
+-- Tabela 10: teaches
+SELECT COUNT(*) as qntRegistros FROM PTBPABD.dbo.teaches;
+-- Número de Registros: 100
+
+
+-- Tabela 11: time_slot
+SELECT COUNT(*) as qntRegistros FROM PTBPABD.dbo.time_slot;
+-- Número de Registros: 20
