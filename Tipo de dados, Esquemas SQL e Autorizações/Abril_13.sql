9b2752c239eede2b7a53db7a709a5b84aff6b6ff
Dominique Jesus <domieduarda2018@gmail.com>
2026-04-13 22:14:47 -0300

    April_13

--- Tipo de dados, Esquemas SQL e Autorizações/Abril_13.sql
+++ Tipo de dados, Esquemas SQL e Autorizações/Abril_13.sql
@@ -1,17 +1,25 @@
 /* Questão 01. Crie as contas de usuário User_A, User_B, User_C, User_D e User_E.*/
 
-CREATE USER User_A WITH PASSWORD = 'userA@PTB';
-CREATE USER User_B WITH PASSWORD = 'userB@PTB';
-CREATE USER User_C WITH PASSWORD = 'userC@PTB';
-CREATE USER User_D WITH PASSWORD = 'userD@PTB';
-CREATE USER User_E WITH PASSWORD = 'userE@PTB';
+CREATE USER User_A WITH PASSWORD = 'userA@PTB123', DEFAULT_SCHEMA = [Student_PT3036472];
+CREATE USER User_B WITH PASSWORD = 'userB@PTB123', DEFAULT_SCHEMA = [Student_PT3036472];
+CREATE USER User_C WITH PASSWORD = 'userC@PTB123', DEFAULT_SCHEMA = [Student_PT3036472];
+CREATE USER User_D WITH PASSWORD = 'userD@PTB123', DEFAULT_SCHEMA = [Student_PT3036472];
+CREATE USER User_E WITH PASSWORD = 'userE@PTB123', DEFAULT_SCHEMA = [Student_PT3036472];
 
 /*Questão 02. Considere o esquema de banco de dados relacional university.
 O User_A poderá selecionar ou modificar qualquer relação, exceto CLASSROOM, 
 e pode conceder qualquer um desses privilégios a outros usuários. */
 
-GRANT SELECT, INSERT, DELETE, UPDATE ON SCHEMA::Student_PT3036472 TO User_A WITH GRANT OPTION;
-REVOKE ALL ON classroom FROM User_A CASCADE;
+GRANT SELECT, INSERT, DELETE, UPDATE ON Student_PT3036472.advisor TO User_A WITH GRANT OPTION;
+GRANT SELECT, INSERT, DELETE, UPDATE ON Student_PT3036472.course TO User_A WITH GRANT OPTION;
+GRANT SELECT, INSERT, DELETE, UPDATE ON Student_PT3036472.department TO User_A WITH GRANT OPTION;
+GRANT SELECT, INSERT, DELETE, UPDATE ON Student_PT3036472.instructor TO User_A WITH GRANT OPTION;
+GRANT SELECT, INSERT, DELETE, UPDATE ON Student_PT3036472.prereq TO User_A WITH GRANT OPTION;
+GRANT SELECT, INSERT, DELETE, UPDATE ON Student_PT3036472.section TO User_A WITH GRANT OPTION;
+GRANT SELECT, INSERT, DELETE, UPDATE ON Student_PT3036472.student TO User_A WITH GRANT OPTION;
+GRANT SELECT, INSERT, DELETE, UPDATE ON Student_PT3036472.takes TO User_A WITH GRANT OPTION;
+GRANT SELECT, INSERT, DELETE, UPDATE ON Student_PT3036472.teaches TO User_A WITH GRANT OPTION;
+GRANT SELECT, INSERT, DELETE, UPDATE ON Student_PT3036472.time_slot TO User_A WITH GRANT OPTION;
 
 /* Questão 03. Liste as permissões do User_A.*/
 
@@ -24,5 +32,6 @@ SELECT princ.name,
 FROM   sys.database_principals princ
 LEFT JOIN sys.database_permissions perm
 ON perm.grantee_principal_id = princ.principal_id
-WHERE princ.type_desc = 'User_A'
+WHERE princ.type_desc = 'SQL_USER'
+AND princ.name = 'User_A'
 ORDER BY princ.name;
